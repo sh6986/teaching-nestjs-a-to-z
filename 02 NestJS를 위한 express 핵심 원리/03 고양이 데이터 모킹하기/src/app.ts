2eadc41488309ad2c4ef7632d4e331d6360898cf
sh6986 <okok960411@gmail.com>
2022-06-11 22:39:04 +0900

    express 미들웨어 이해하기

--- 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/app.ts
+++ 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/app.ts
@@ -3,13 +3,27 @@ import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
-const data = [1, 2, 3, 4];
-
 app.get('/', (req: express.Request, res: express.Response) => {
-    console.log(req);
-    res.send({ cats: Cat });
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat, blue: Cat[0] });
+});
+
+app.get('/cats/som', (req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('this is logging middleware');
+  res.send({ error: '404 not found error' });
 });
 
 app.listen(8000, () => {
-    console.log('server is on...');
+  console.log('server is on...');
 });
