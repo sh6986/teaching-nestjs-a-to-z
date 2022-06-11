fa142f862e76e1bf6a5c7d271b64ab41a0c96117
sh6986 <okok960411@gmail.com>
2022-06-11 23:52:58 +0900

    고양이 데이터 Update Delete API 개발

--- 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/cats/cats.route.ts
+++ 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/cats/cats.route.ts
@@ -61,4 +61,84 @@ router.post('/cats', (req, res) => {
   }
 });
 
+//* UPDATE 고양이 데이터 업데이트 -> PUT
+router.put('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    const cat = Cat.find((cat) => {
+      return (cat.id = params.id);
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error,
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
+router.patch('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+
+    const cat = Cat.find((cat) => {
+      return (cat.id = params.id);
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error,
+    });
+  }
+});
+
+//* DELETE 고양이 데이터 삭제 -> DELETE
+router.delete('/cats/:id', (req, res) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error,
+    });
+  }
+});
+
 export default router;
