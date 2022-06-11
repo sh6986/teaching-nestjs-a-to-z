3b05d80570711cba35c0910dd91c7c037283d380
sh6986 <okok960411@gmail.com>
2022-06-12 04:02:41 +0900

    Modules & 캡슐화

--- 04 NestJS 개요 및 객체지향 디자인 패턴/src/app.controller.ts
+++ 04 NestJS 개요 및 객체지향 디자인 패턴/src/app.controller.ts
@@ -2,15 +2,12 @@ import { Body, Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 
-@Controller('cats')
+@Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id/:name')
-  getHello(@Req() req: Request, @Body() Body, @Param() param): string {
-    // console.log(req);
-    // console.log(param);
-    // return 'hello world';
-    return this.appService.getHello(Body, param);
+  @Get()
+  getHello() {
+    return 'hello world';
   }
 }
