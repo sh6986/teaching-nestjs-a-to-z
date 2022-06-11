3de4130b16d7fb6d98b010c8cdd52bdc5c2bd61d
sh6986 <okok960411@gmail.com>
2022-06-11 23:39:53 +0900

    고양이 route 분리, 모듈화

--- 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/app.ts
+++ 02 NestJS를 위한 express 핵심 원리/03 고양이 데이터 모킹하기/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.route';
 
 const app: express.Express = express();
 
@@ -13,73 +13,12 @@ app.use((req, res, next) => {
 //* json middleware
 app.use(express.json());
 
-//* READ 고양이 전체 데이터 다 조회
-app.get('/cats', (req, res) => {
-  try {
-    const cats = Cat;
-    // throw new Error('db connect error');
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error,
-    });
-  }
-});
-
-//* READ 특정 고양이 데이터 조회
-app.get('/cats/:id', (req, res) => {
-  try {
-    const params = req.params;
-    const cat = Cat.find((cat) => {
-      return (cat.id = params.id);
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가 api
-app.post('/cats', (req, res) => {
-  try {
-    const data = req.body;
-    Cat.push(data);
-
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error,
-    });
-  }
-});
+app.use(catsRouter);
 
 //* 404 middleware
 app.use((req, res, next) => {
   console.log('this is logging middleware');
   res.send({ error: '404 not found error' });
-});
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ cats: Cat });
 });
 
 app.listen(8000, () => {
