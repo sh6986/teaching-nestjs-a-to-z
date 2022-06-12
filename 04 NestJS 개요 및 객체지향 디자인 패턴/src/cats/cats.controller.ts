e3540ad73bfee1c50ec56727ff2b3707e99a6828
sh6986 <okok960411@gmail.com>
2022-06-12 16:35:28 +0900

    Exception filter & Pipes

--- 04 NestJS 개요 및 객체지향 디자인 패턴/src/cats/cats.controller.ts
+++ 04 NestJS 개요 및 객체지향 디자인 패턴/src/cats/cats.controller.ts
@@ -1,19 +1,36 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   // cats/
   @Get()
   getAllCat() {
+    throw new HttpException('api broken', 401);
     return 'all cat';
   }
 
   // cats/:id
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
     return 'one cat';
   }
 
