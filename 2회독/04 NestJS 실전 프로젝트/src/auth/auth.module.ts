e5f8576d89cbee1d29eb716311e491a2b5037287
sh6986 <okok960411@gmail.com>
2022-07-31 21:15:00 +0900

    Swagger API 보안 설정 & 로그인 API 프론트엔드와 연결

--- 2회독/04 NestJS 실전 프로젝트/src/auth/auth.module.ts
+++ 2회독/04 NestJS 실전 프로젝트/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { CatsModule } from 'src/cats/cats.module';
@@ -7,11 +8,12 @@ import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }), // 인증 관련 설정
 
     // 로그인 관련 설정(JWT를 만들어주는 모듈)
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1y' },
     }),
 
--- 2회독/04 NestJS 실전 프로젝트/src/auth/jwt/jwt.strategy.ts
+++ 2회독/04 NestJS 실전 프로젝트/src/auth/jwt/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly catsRepository: CatsRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 헤더의 토큰으로부터 추출한다.
-      secretOrKey: 'secret',
+      secretOrKey: process.env.JWT_SECRET,
       ignoreExpiration: false,
     });
   }
